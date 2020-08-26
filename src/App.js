291b547d6af627c27c0eb798984dacdb75e15b47
specter25 <ujjwalcoding012@gmail.com>
2020-08-26 11:32:13 +0530

    Navbar setup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React ,{Component} from 'react';
 import Web3 from 'web3'
 import './App.css';
 import Marketplace from '../src/abis/Marketplace.json'
+import Navbar from './components/Navbar'
 
 class App extends Component {
 
@@ -59,6 +60,7 @@ class App extends Component {
   render( ) {
     return (
       <div className="App">
+        <Navbar />
        <p>{this.state.account}</p>
       </div>
     );
