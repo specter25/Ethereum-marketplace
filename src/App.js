5af2cc9587d4febfd05c62dd703289516e16c969
specter25 <ujjwalcoding012@gmail.com>
2020-08-26 10:29:45 +0530

    web3 setup in the frontend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React ,{Component} from 'react';
+import Web3 from 'web3'
 import './App.css';
+import Marketplace from '../src/abis/Marketplace.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+  async loadBlockchainData() {
+    const web3=window.web3 ;
+    //load accounts
+     const accounts = await web3.eth.getAccounts()
+     console.log(accounts[0])
+     this.setState({account:accounts[0]}) 
+     
+     const networkId = await web3.eth.net.getId() 
+     const networkData = Marketplace.networks[networkId]
+     let marketplace
+     if(networkData)
+     {
+      const abi = Marketplace.abi
+       marketplace = new  web3.eth.Contract(abi,networkData.address )
+     }
+     else {
+      window.alert('Marketplace contract not deployed to the public network')
+     }
+
+     console.log(marketplace)
+
+     
+  }
+
+  constructor(props)
+  {
+    super(props)
+    this.state ={
+      account:'',
+      productCount:0,
+      products:[],
+      loading:true
+    }
+  }
+  render( ) {
+    return (
+      <div className="App">
+       <p>{this.state.account}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
